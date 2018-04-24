03da27a3abee5425bad6f9ceb98e1e0ad244930b
sumit <kumar1993sumit15@gmail.com>
2018-04-24 14:26:25 +0530

    use rooms class to handle incoming requests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = require('express')()
 var http = require('http').Server(app)
 var path = require('path')
 var io = require('socket.io')(http)
+var Handler = new (require('./js/rooms'))(io)
 
 app.use(express.static('public'))
 
@@ -12,38 +13,35 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   console.log('a client connected')
-  socket.on('joinroom', function (data) {
-    console.log('joining room')
-    let roomId = data.roomId
-    socket.join(roomId, err => {
-      if (err != null) console.log(err)
-      io.to(roomId).emit('broadcast', `${data.user} has joined the room`)
-    })
-    socket.on('disconnect', function (reason) {
-      let rooms = Object.keys(socket.rooms)
-      console.log('disconnected', rooms)
-      io.to(rooms).emit('broadcast', `${data.name} has disconnected`)
-    })
+  socket.on('disconnect', function (reason) {
+    let rooms = Object.keys(socket.rooms)
+    console.log('disconnected', rooms)
+    io.to(rooms[0]).emit('broadcast', ` has disconnected`)
   })
 
   socket.on('request', function (data) {
+    // be careful of while passing data, stringified/json
     let req = JSON.parse(data)
     switch (req.type) {
+      case 'join-room':
+        console.log('on server joining room')
+        Handler.joinRoom(socket, req)
+        break
       case 'ice-candidate':
         console.log('received candidate on server')
-        socket.broadcast.emit('receiver', data)
+        Handler.relayIceCandidate(socket, req)
         break
       case 'video-offer':
         console.log('new offer on server')
-        socket.broadcast.emit('receiver', data)
+        Handler.relayVideoOffer(socket, req)
         break
       case 'video-answer':
         console.log('answer received on server')
-        socket.broadcast.emit('receiver', data)
+        Handler.relayVideoAnswer(socket, req)
     }
   })
 })
 
 http.listen(process.env.PORT || 3000, function () {
-  console.log('listening on *:3000')
+  console.log('Server Started')
 })
