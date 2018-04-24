9df5eb19b4f0662619a6e066b90831e0082443f0
sumit <kumar1993sumit15@gmail.com>
2018-04-24 14:25:32 +0530

    fixed issues of data format

--- js/rooms.js
+++ js/rooms.js
@@ -1,48 +1,65 @@
-export class ConnectionHandler {
+module.exports = class ConnectionHandler {
   constructor (io) {
     this.ioConn = io
     this.connections = {}
     this.activeRooms = []
   }
+
   getRoom (socket) {
     return Object.keys(socket.rooms)[0]
   }
 
-  getSocket (room, data) {
-    return this.connections[room][data.target]
+  getSocket (room, target) {
+    return this.connections[room][target]
   }
-  reply (room, username, message) {
-    // send any message to a user
-  }
-  addUser (socket, data) {
+
+  joinRoom (socket, data) {
     let roomId = data.roomId
     let room = this.connections[roomId]
-    if (Object.keys(room).length >= 10) {
-      // cannot join room
+    if (room != null) {
+      if (Object.keys(room).length >= 10) {
+        // cannot join room
+        socket.emit('broadcast', `${data.roomId} is already full`)
+      } else {
+        socket.join(roomId, err => {
+          if (err != null) console.log(err)
+          else {
+            room[data.name] = socket
+            this.ioConn.to(roomId).emit('broadcast', `${data.name} has joined the room`)
+          }
+        })
+      }
     } else {
-      room[data.name] = socket
+      socket.join(roomId, err => {
+        if (err != null) console.log(err)
+        else {
+          this.connections[roomId] = {}
+          this.connections[roomId][data.name] = socket
+          this.ioConn.to(roomId).emit('broadcast', `${data.name} has joined the room`)
+        }
+      })
     }
   }
 
   relayToReceiver (socket, data) {
-    socket.emit('receiver', data)
+    socket.emit('receiver', JSON.stringify(data))
   }
 
   relayIceCandidate (socket, data) {
     let room = this.getRoom(socket)
-    let targetSocket = this.getSocket(room, data)
+    let targetSocket = this.getSocket(room, data.target)
     this.relayToReceiver(targetSocket, data)
   }
 
   relayVideoOffer (socket, data) {
     let room = this.getRoom(socket)
-    let targetSocket = this.getSocket(room, data)
+    let targetSocket = this.getSocket(room, data.target)
     this.relayToReceiver(targetSocket, data)
   }
 
   relayVideoAnswer (socket, data) {
     let room = this.getRoom(socket)
-    let targetSocket = this.getSocket(room, data)
+    let targetSocket = this.getSocket(room, data.target)
     this.relayToReceiver(targetSocket, data)
   }
 
